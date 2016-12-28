2917021fe42673953806059889f6576bc3c3f532
kyoko020208 <kyoko020208@gmail.com>
2016-12-28 14:09:40 +0900

    Fix 諸々の機能修正
    「3項以上の演算」「桁あふれ処理 」「表示の時に、四捨五入する」

--- Dentaku3/Calc.swift
+++ Dentaku3/Calc.swift
@@ -15,143 +15,153 @@ class Calc {
     //ユーザが押した演算子
     var InputSym: String = ""
     
-    //左項
-    var InputL: String = ""
+    //項 (一つの式で入力できるのは3項まで)
+    var numbersString: [String] = ["", "", ""]
     
-    //右項
-    var InputR: String = ""
+    //演算子（一つの式で入力できる演算子は2つまで)
+    var symbolsString: [String] = ["", ""]
+    
+    //項と演算子の番号
+    var i:Int = 0
     
     //何も入力されていない時はfalse
     //数値もしくは演算子が押されるとtrueに変わる
     var userInputting: Bool = false
     
+    //最初の2項の計算結果
+    var subResultValue: Double = 0.0
+    
     //結果の値
     var resultValue: Double = 0.0
-    
-    //演算子ボタンを押した後かどうか
-    var afterCalc: Bool = false
     
     
     //数字と小数点の入力処理
     func makeNum() {
         
-        //最初の数値が小数点なら"0."に変換
-        if userInputting == false {
-            userInputting = true
-            if InputNum == "."{
-                InputNum = "0."
+        //桁あふれ処理のため、数字の長さをlenに格納
+        let len: Int = numbersString[i].characters.count
+        
+        //入力できるのは3項まで
+        if i < 3 {
+            
+            //最初の数値が小数点なら"0."に変換
+            if userInputting == false {
+                userInputting = true
+                if InputNum == "."{
+                    InputNum = "0."
+                }
             }
-        }
-        
-        
-        //項に値を設定していく
-        //左項
-        if afterCalc == false { //1つの項に小数点が複数入らないよう処理
-            if InputL.range(of:".") != nil && InputNum == "."{
+            
+            
+            //1つの項に小数点が複数入らないよう処理
+            if numbersString[i].range(of:".") != nil && InputNum == "."{
                 InputNum = ""
             }
             
-            if InputL == "0" { //最初の値が0なら次桁を追加しない
+            if numbersString[i] == "0" { //最初の値が0なら次桁を追加しない
                 if InputNum == "." {//ただし小数点の場合はOK
-                    InputL = "0."
+                    numbersString[i] = "0."
                 } else {
-                    InputL = InputNum
-                    print(InputL)
+                    numbersString[i] = InputNum
+                    print(numbersString[i])
                 }
-            } else {
-                InputL += InputNum
-                print(InputL)
+            } else if len < 7 { //6桁もう入っていれば、新たに数値は入力できない
+                numbersString[i] += InputNum
+                print(numbersString[i])
             }
-        }
-            
-            
-            //右項
-        else {
-            if InputR.range(of:".") != nil && InputNum == "."{
-                InputNum = ""
-            }
-            
-            if InputR == "0" {
-                if InputNum == "." {
-                    InputR = "0."
-                } else {
-                InputR = InputNum
-                print(InputR)
-                }
-            } else {
-                InputR += InputNum
-                print(InputR)
-            }
+        } else {
+            InputNum = ""
         }
     }
     
     
     func makeSym() {
-        //前に数値を入力していなかったら演算子は入力できない
-        if userInputting == false {
+        
+        if i < 2 {
+            //前に数値を入力していなかったら演算子は入力できない
+            if userInputting == false {
+                InputSym = ""
+            } else {
+                symbolsString[i] = InputSym
+                userInputting = false
+                i += 1
+            }
+        } else {
             InputSym = ""
         }
-        afterCalc = true
-        userInputting = false
+    }
+    
+    
+    
+    
+    func Operations (currentSym: String, X: String, Y: String) -> Double{
+        
+        switch currentSym {
+        case "+":
+            return Double(X)! + Double(Y)!
+        case "-":
+            return Double(X)! - Double(Y)!
+        case "×":
+            return Double(X)! * Double(Y)!
+        case "÷":
+            if Y == "0" { //右項が0なら割り算はできない
+                print("error")
+                return 0.0
+            } else {
+                return Double(X)! / Double(Y)!
+            }
+        default: return 0.0
+        }
     }
     
     
     
     func makeResult () {
         //演算子によって条件分け
-        switch InputSym {
-        case "+":
-            resultValue = Double(InputL)! + Double(InputR)!
-        case "-":
-            resultValue = Double(InputL)! - Double(InputR)!
-        case "×":
-            resultValue = Double(InputL)! * Double(InputR)!
-        case "÷":
-            if InputR == "0" { //右項が0なら割り算はできない
-                print("error")
-                return
+        if i == 2 { //項が3つある場合
+            if symbolsString[1] == "×" || symbolsString[1] == "÷"{ //2つ目の演算子が"×"か"÷"ならば
+                //先に項2と項3を計算→subResultValueに格納
+                subResultValue = Operations(currentSym: symbolsString[1], X: numbersString[1] , Y: numbersString[2])
+                resultValue = Operations(currentSym: symbolsString[0], X: numbersString[0], Y: String(subResultValue))
             } else {
-                resultValue = Double(InputL)! / Double(InputR)!
+                subResultValue = Operations(currentSym: symbolsString[0], X: numbersString[0], Y: numbersString[1])
+                resultValue = Operations(currentSym: symbolsString[1], X: String(subResultValue), Y: numbersString[2])
             }
-        default: break
+        } else if i == 1 { //項が2つのみの場合
+            resultValue = Operations(currentSym: symbolsString[0], X: String(numbersString[0]), Y: numbersString[1])
+        } else {
+            resultValue = Double(numbersString[0])!
         }
-        
-        //初期値に戻す
-        afterCalc = false
         userInputting = false
     }
     
     
-    
-    func deleteOne() {
-        //左項しか入力されていなければ、左項から一字消す
-        if afterCalc == false {
-            let _InputL :NSMutableString = NSMutableString(string: InputL)
-            _InputL.deleteCharacters(in: NSRange(location: _InputL.length - 1, length: 1))
-            InputL = String(_InputL)
-        }
-            
-            //右項が入力されていれば、右項から一字消す
-        else {
-            let _InputR :NSMutableString = NSMutableString(string: InputR)
-            _InputR.deleteCharacters(in: NSRange(location: _InputR.length - 1, length: 1))
-            InputR = String(_InputR)
-            
-        }
-        
+    func deleteNumber() {
+        let _numbersString :NSMutableString = NSMutableString(string: numbersString[i])
+        _numbersString.deleteCharacters(in: NSRange(location: _numbersString.length - 1, length: 1))
+        numbersString[i] = String(_numbersString)
     }
     
+    func deleteSymbol() {
+        symbolsString[i-1] = ""
+        i = i - 1
+        userInputting = true
+    }
     
     
     func deleteAll() {
         //Cボタンを押すと値入力が一気にクリアされる
         //ただし計算結果は残る
-        InputR = ""
-        InputL = ""
         InputNum = ""
         InputSym = ""
+        for i in 0...1 {
+            numbersString[i] = ""
+            symbolsString[i] = ""
+            numbersString[2] = ""
+            print(numbersString[i])
+        }
         userInputting = false
-        afterCalc = false
+        i = 0
     }
     
     
--- Dentaku3/ViewController.swift
+++ Dentaku3/ViewController.swift
@@ -34,17 +34,7 @@ class ViewController: UIViewController {
         calc.makeNum()
         
         //Formulaに出力
-        if calc.InputL == "0" {
-            if calc.InputNum != "." { //"01"のような出力を避ける
-                Formula.text = calc.InputNum
-            }
-        } else if calc.InputR == "0" {
-            if calc.InputNum != "." {
-                Formula.text = calc.InputL + calc.InputSym + calc.InputNum
-            }
-        } else {
-            Formula.text = calc.InputL + calc.InputSym + calc.InputR
-        }
+        Formula.text = calc.numbersString[0] + calc.symbolsString[0] + calc.numbersString[1] + calc.symbolsString[1] + calc.numbersString[2]
     }
     
     
@@ -52,27 +42,51 @@ class ViewController: UIViewController {
     @IBAction func Symbol(_ sender: UIButton) {
         calc.InputSym = (sender.titleLabel?.text)!
         calc.makeSym()
-        Formula.text = Formula.text! + calc.InputSym
         
+        //Formulaに出力
+        Formula.text = Formula.text! + calc.InputSym
     }
     
     
     @IBAction func Calculate(_ sender: UIButton) {
         calc.makeResult()
-        Result.text = String(calc.resultValue)
+        
+        //Resultに出力(小数点以下は切り捨て
+        Result.text = String(Int(calc.resultValue))
     }
+    
     
     @IBAction func DeleteOne(_ sender: UIButton) {
         
-        //式から1字消す
+        //Formulaに出力されている文字列を１文字ずつに分解
         let _Formula :NSMutableString = NSMutableString(string: Formula.text!)
+        
+        //「+, -, *, /」をUNint32に変換
+        let plus: UInt32 = (String("+")?.unicodeScalars.first!.value)!
+        let minus: UInt32 = (String("-")?.unicodeScalars.first!.value)!
+        let kakeru: UInt32 = (String("×")?.unicodeScalars.first!.value)!
+        let waru: UInt32 = (String("÷")?.unicodeScalars.first!.value)!
+        
+        //Formulaに出力されている文字列の最後の文字をFINに格納
+        let FIN: UInt32 = UInt32(_Formula.character(at: _Formula.length-1))
+        
+        //Formulaの文字列の最後の文字が演算子ならdeleteSymbolを実行
+        if  plus == FIN || minus == FIN || kakeru == FIN || waru == FIN {
+            calc.deleteSymbol()
+        } else { //最後の文字が数字ならdeleteNumberを実行
+            calc.deleteNumber()
+        }
+        
+        //Formulaの文字列からも１文字削除して、出力し直す
         _Formula.deleteCharacters(in: NSRange(location: _Formula.length - 1, length: 1))
         Formula.text = String(_Formula)
     }
     
     
     @IBAction func DeleteAll(_ sender: UIButton) {
-        //式を全クリア
+        
+        //全クリア
+        calc.deleteAll()
         Formula.text = ""
     }
     
